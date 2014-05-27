a1b2b8b8bb4a3c461c151aa3d231290363cdc22e
arbuztw <arbuz.tw@gmail.com>
2014-05-27 15:34:34 +0800

    done hw6_2_1 hw6_2_2

---
+++ hw6_2_1.c
@@ -0,0 +1,135 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "avl.h"
+#include "bst.h"
+#include "rb.h"
+
+typedef struct
+{
+	char data[10];
+} Str;
+
+void postorder_integer_avl(const struct avl_node *node)
+{
+	if (!node) return ;
+	
+	printf("%d ", *((int *)node->avl_data));
+	if (node->avl_link[0] || node->avl_link[1])
+	{
+		putchar('(');
+		postorder_integer_avl(node->avl_link[0]);
+		putchar(',');
+		putchar(' ');
+		postorder_integer_avl(node->avl_link[1]);
+		putchar(')');
+	}
+}
+
+void inorder_bst(const struct bst_node *node)
+{
+	if (!node) return ;
+	inorder_bst(node->bst_link[0]);
+	printf("%s ", ((Str *)node->bst_data)->data);
+	inorder_bst(node->bst_link[1]);
+}
+
+void preorder_str_avl(const struct avl_node *node)
+{
+	if (!node) return ;
+	
+	
+	printf("%s ", ((Str *)node->avl_data)->data);
+	if (node->avl_link[0] || node->avl_link[1])
+	{
+		putchar('(');
+		preorder_str_avl(node->avl_link[0]);
+		putchar(',');
+		putchar(' ');
+		preorder_str_avl(node->avl_link[1]);
+		putchar(')');
+	}
+}
+
+void preorder_str_bst(const struct bst_node *node)
+{
+	if (!node) return ;
+	
+	
+	printf("%s ", ((Str *)node->bst_data)->data);
+	if (node->bst_link[0] || node->bst_link[1])
+	{
+		putchar('(');
+		preorder_str_bst(node->bst_link[0]);
+		putchar(',');
+		putchar(' ');
+		preorder_str_bst(node->bst_link[1]);
+		putchar(')');
+	}
+}
+
+void preorder_str_rb(const struct rb_node *node)
+{
+	if (!node) return ;
+	
+	
+	printf("%s ", ((Str *)node->rb_data)->data);
+	if (node->rb_link[0] || node->rb_link[1])
+	{
+		putchar('(');
+		preorder_str_rb(node->rb_link[0]);
+		putchar(',');
+		putchar(' ');
+		preorder_str_rb(node->rb_link[1]);
+		putchar(')');
+	}
+}
+
+int int_compare(const void *pa, const void *pb, void *param)
+{
+	int a = *(const int *)pa;
+	int b = *(const int *)pb;
+
+	if (a < b) return -1;
+	else if (a > b) return 1;
+	else return 0;
+}
+
+int str_compare(const void *p1, const void *p2, void *param)
+{
+	return strcmp(((const Str *)p1)->data, ((const Str *)p2)->data);
+}
+
+int main()
+{
+	char names[32][10] = {
+		"Chuang", "Chou", "Chang", "Chao", "Chen", "Cheng", "Chu", "Fu", "Fuh", "Hsiang", "Hsu", "Hsueh", "Hung", 
+		"Jang", "Kao", "Kuo", "Lai", "Lee", "Liao", "Lin", "Liou", "Liu", "Lu", "Lyuu", "Ouhyoung", "Oyang", "Shih", 
+		"Tsai", "Tseng", "Wang", "Wu", "Yang" };
+	struct avl_table *atree;
+	struct bst_table *btree;
+	struct rb_table  *rtree;
+
+	atree = avl_create(str_compare, NULL, NULL);
+	btree = bst_create(str_compare, NULL, NULL);
+	rtree = rb_create(str_compare, NULL, NULL);
+
+	int i;
+	for (i = 0; i < 32; i++)
+	{
+		Str *ele = (Str *)malloc(sizeof(Str));
+		strcpy(ele->data, names[i]);
+		avl_probe(atree, ele);
+		bst_probe(btree, ele);
+		rb_probe(rtree, ele);
+	}
+
+	preorder_str_avl(atree->avl_root);
+	puts("");
+	preorder_str_bst(btree->bst_root);
+	puts("");
+	preorder_str_rb(rtree->rb_root);
+	puts("");
+
+	return 0;
+}
---
+++ hw6_2_2.c
@@ -0,0 +1,112 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "avl_ntudsa.h"
+#define N 1000005
+
+typedef struct
+{
+	char name[25];
+	int score[5];
+	int avg;
+} Student;
+
+void inorder(const struct avl_node *node, int check, int idx)
+{
+	if (!node) return ;
+	if (check != node->avl_cnode[0] + node->avl_cnode[1] + 1)
+	{
+		printf("%d ", idx);
+		puts("jizz");
+	}
+	//putchar('(');
+	inorder(node->avl_link[0], node->avl_cnode[0], idx*2+1);
+	//printf(" %d, %d ", node->avl_cnode[0], node->avl_cnode[1]);
+	//putchar('');
+	//printf("%s ", ((Student *)node->avl_data)->name);
+	inorder(node->avl_link[1], node->avl_cnode[1], idx*2+2);
+	//putchar(')');
+}
+
+Student *findRank(const struct avl_node *node, int r)
+{
+	int c = node->avl_cnode[0];
+/*
+	while (r != c + 1)
+	{
+		//printf("(%d, %d, %d) ", r, c, node->avl_cnode[1]);
+		if (r <= c)
+			node = node->avl_link[0];
+		else
+		{
+			node = node->avl_link[1];
+			r -= (c + 1);
+		}
+		if (!node) puts("jizz");
+		fflush(stdout);
+		c = node->avl_cnode[0];
+	}
+	return (Student *)node->avl_data;
+*/
+//	printf("%d ", c);
+	if (r == c + 1)
+		return (Student *)node->avl_data;
+	else if (r <= c)
+		return findRank(node->avl_link[0], r);
+	else
+		return findRank(node->avl_link[1], r - c - 1);
+
+}
+
+int cmp(const void *pa, const void *pb, void *param)
+{
+	const Student *a = (const Student *)pa;
+	const Student *b = (const Student *)pb;
+
+
+	if (a->avg != b->avg) return -(a->avg - b->avg);
+	else if (a->score[1] != b->score[1]) return -(a->score[1] - b->score[1]);
+	else if (a->score[2] != b->score[2]) return -(a->score[2] - b->score[2]);
+	else if (a->score[4] != b->score[4]) return -(a->score[4] - b->score[4]);
+	else if (a->score[3] != b->score[3]) return -(a->score[3] - b->score[3]);
+	else if (a->score[0] != b->score[0]) return -(a->score[0] - b->score[0]);
+	else return strcmp(a->name, b->name);
+
+	return strcmp(a->name, b->name);
+}
+
+int main()
+{
+	int i, x, cnt = 0, sum;
+	char cmd[10];
+	struct avl_table *tree = avl_create(cmp, NULL, NULL);
+	Student *s;
+
+	while (~scanf("%s", cmd))
+	{
+		cnt++;
+		if (cmd[0] == 'p')
+		{
+			s = (Student *)malloc(sizeof(Student));
+			scanf("%s", s->name);
+			sum = 0;
+			for (i = 0; i < 5; i++)
+			{
+				scanf("%d", &s->score[i]);
+				sum += s->score[i];
+			}
+			s->avg = sum / 5;
+			avl_probe(tree, s);
+		}
+		else
+		{
+			scanf("%d", &x);
+			s = findRank(tree->avl_root, x);
+			//puts("");
+			printf("%s\n", s->name);
+		}
+	}
+	//inorder(tree->avl_root, cnt-1, 0);
+	//puts("");
+	return 0;
+}
